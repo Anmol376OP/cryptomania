c831f3d5c8a8ae4e15d1ee15f3f556272083b464
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-02-06 15:54:49 +0530

    Changed api for news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,19 +3,27 @@ import { useEffect, useState } from 'react'
 import '../styles/NewsItem.css'
 import axios from 'axios'
 import Input from 'rc-input';
+
+
 const News = () => {
     const [user, setUser] = useState([]);
-    const [maxIndex, setMaxIndex] = useState(15);
-    const [x, setX] = useState([])
-    const fetchData = async () => {
-        const response = await axios.get("https://newsapi.org/v2/everything?q=crypto&apiKey=75e5c75aa2a84d919c90f7bbd67b5c1f&page=2");
-        setUser(response.data);
-        setX(user?.articles?.slice(5, maxIndex));
-    }
-
+    const options = {
+        method: 'GET',
+        url: 'https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-news',
+        params: { pair_ID: '1057391', page: '1', time_utc_offset: '28800', lang_ID: '1' },
+        headers: {
+            'X-RapidAPI-Key': '878f0a0e71mshf8398d7f495ec97p11d493jsn8e6523887d71',
+            'X-RapidAPI-Host': 'investing-cryptocurrency-markets.p.rapidapi.com'
+        }
+    };
     useEffect(() => {
-        fetchData();
-    }, [user, maxIndex])
+        axios.request(options).then(function (response) {
+            setUser(response.data.data[0].screen_data.news);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }, [])
+    console.log(user)
 
     if (user) { } else return (<div>
         <h2 style={{ height: '100vh', color: 'white' }}>Loading....</h2>
@@ -25,15 +33,14 @@ const News = () => {
         <div className='News-outbox'>
             {/* <Input placeholder='Search' onChange={(e) => setMaxIndex(e.target.value)} /> */}
             <div className='NewsItemContainer'>
-                {x ? x.map((index) => (
+                {user ? user.map((index) => (
                     <div className="card">
-                        <h4><a target='_blank' href={index.url ? index.url : ''} style={{ textDecoration: 'none', color: 'white' }}>{index.title ? index.title : 'Title'}</a></h4>
-                        <a target='_blank' href={index.url ? index.url : ''} style={{ textDecoration: 'none', color: 'white' }}><i className="fas fa-arrow-right"></i></a>
-                        <p>{index.author ? index.author.substring(0, 25) : 'Anonymous'}</p>
-                        <p>{index.publishedAt ? index.publishedAt : 'Loading ...'}</p>
-                        <div className="pic" style={index.urlToImage ? { backgroundImage: `url(${index.urlToImage})` } : ''} alt=''></div>
-                        <button className='ExpandingButton'>
-                        </button>
+                        <h4><a target='_blank' href={index.news_link ? index.news_link : ''} style={{ textDecoration: 'none', color: 'white' }}>{index.HEADLINE ? index.HEADLINE : 'Title'}</a></h4>
+                        <a target='_blank' href={index.news_link ? index.news_link : ''} style={{ textDecoration: 'none', color: 'white' }}><i className="fas fa-arrow-right"></i></a>
+                        <p>{index.news_provider_name ? index.news_provider_name.substring(0, 25) : 'Anonymous'}</p>
+                        <p>{index.last_updated ? index.last_updated : 'Loading ...'}</p>
+                        <div className="pic" style={index.related_image_big ? { backgroundImage: `url(${index.related_image_big})` } : ''} alt=''></div>
+                        <button className='ExpandingButton'></button>
                     </div>
                 )) : ''}
             </div>
